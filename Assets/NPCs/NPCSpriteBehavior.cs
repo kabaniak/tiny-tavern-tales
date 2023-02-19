3331542938ca1c1e57e87f57a73059b3618df399
asmithat7 <austinsmith2023@u.northwestern.edu>
2023-02-19 14:53:25 -0600

    Fixed Leaving and OrderTracker Bug

--- Assets/NPCs/NPCSpriteBehavior.cs
+++ Assets/NPCs/NPCSpriteBehavior.cs
@@ -122,6 +122,7 @@ public class NPCSpriteBehavior : MonoBehaviour
             {
                 angry = true;
                 leaveTable();
+                GameObject.FindObjectOfType<OrderTracker>().removeMyOrder(gameObject);
                 currentState = "leaving";
             }
         }
