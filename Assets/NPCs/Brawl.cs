acfc4738a4bd6f00e62aeac3b44c2a0d2027084b
kabaniak <kabaniak@gmail.com>
2023-02-27 14:48:57 -0600

    brawl creates messes

--- Assets/NPCs/Brawl.cs
+++ Assets/NPCs/Brawl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Brawl : MonoBehaviour
 {
     GameObject[] involvedNPCs = { null, null };
+    public GameObject messPrefab;
 
     bool p1Interacting = false;
     bool p2Interacting = false;
@@ -18,11 +19,14 @@ public class Brawl : MonoBehaviour
     float radius = 7;
     Vector2 circleCenter;
     Vector2 startDir;
-
+    bool clockwise;
     float currAngle;
 
     float nextDam;
+    int sinceMess = 0;
+    int messInt = 3;
     float interval = 2.5f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +39,9 @@ public class Brawl : MonoBehaviour
         startDir.Normalize();
 
         nextDam = Time.time + interval;
+
+        if(Random.value < 0.5) { clockwise = true; }
+        else { clockwise = false; }
 
     }
 
@@ -91,14 +98,23 @@ public class Brawl : MonoBehaviour
 
         if(Time.time > nextDam)
         {
+            if(sinceMess >= messInt)
+            {
+                createMess();
+                sinceMess = 0;
+            }
             GameObject.FindObjectOfType<GameManager>().updateRep(-1);
+
+            if(Random.value < 0.2) { clockwise = !clockwise; }
             nextDam = Time.time + interval;
+            sinceMess++;
         }
     }
 
     private void FixedUpdate()
     {
         moveAround();
+
     }
 
     public void startBrawl(GameObject npc1, GameObject npc2)
@@ -142,7 +158,8 @@ public class Brawl : MonoBehaviour
         }
         else
         {
-            currAngle += 0.02f;
+            if (clockwise) { currAngle -= 0.02f; }
+            else { currAngle += 0.02f; }
 
             float newXPos = circleCenter.x + Mathf.Cos(currAngle) * radius;
             float newYPos = circleCenter.y + Mathf.Sin(currAngle) * radius;
@@ -158,5 +175,24 @@ public class Brawl : MonoBehaviour
 
             transform.position = new Vector3(newXPos, newYPos, 0);
         }
+    }
+
+    void createMess()
+    {
+        Vector2 fromCenter = new Vector2(transform.position.x - circleCenter.x, transform.position.y - circleCenter.y);
+        fromCenter.Normalize();
+        float xPos = transform.position.x + fromCenter.x * ( Random.Range(-0.5f, 10)) + Random.Range(-2f, 2f);
+        float yPos = transform.position.y + fromCenter.y * ( Random.Range(-0.5f, 6)) + Random.Range(-2f, 2f);
+
+        if (yPos < -15)
+        {
+            yPos = -15 + Random.Range(0, 2f) ;
+        }
+        else if (yPos > 15)
+        {
+            yPos = 15 + Random.Range(-2f, 0);
+        }
+        GameObject messNew = Instantiate(messPrefab, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        messNew.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 0f, 0.6f, 0.9f, 1.0f);
     }
 }
